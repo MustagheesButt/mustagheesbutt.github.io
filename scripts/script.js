324edbd222d78c146210ce22739cfbd223fb8765
Mustaghees Butt <renaldpirate@gmail.com>
2015-09-23 22:27:57 +0500

    Animations added
    Animations related to menu and menu items were added.

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,22 @@
 $(document).ready(function () {
 	
+	// nav menu controller
 	$("nav").click(function () {
 		$("body").animate({scrollTop: "0px"}, 300);
 		$("body").toggleClass("menu-open");
+	});
+	
+	// nav menu item hover animation
+	$("#menu ul li:first-child").mouseover(function () {
+		$("#menu").addClass("red");
+	});
+	$("#menu ul li:first-child").mouseleave(function () {
+		$("#menu").removeClass("red");
+	});
+	$("#menu ul li:last-child").mouseover(function () {
+		$("#menu").addClass("green");
+	});
+	$("#menu ul li:last-child").mouseleave(function () {
+		$("#menu").removeClass("green");
 	});
 }); 
