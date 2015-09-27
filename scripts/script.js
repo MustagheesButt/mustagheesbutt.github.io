1842baae88581d79822a6f1f63968431829d8f8d
Mustaghees Butt <renaldpirate@gmail.com>
2015-09-27 13:07:33 +0500

    Major design Changes - progress
    Progress in major design changes. Still Incomplete

--- scripts/script.js
+++ scripts/script.js
@@ -1,17 +1,3 @@
 $(document).ready(function () {
 	
-	// nav menu item hover animation
-	$("#menu ul li:first-child").mouseover(function () {
-		$("#menu").addClass("red");
-	});
-	$("#menu ul li:first-child").mouseleave(function () {
-		$("#menu").removeClass("red");
-	});
-	$("#menu ul li:last-child").mouseover(function () {
-		$("#menu").addClass("green");
-	});
-	$("#menu ul li:last-child").mouseleave(function () {
-		$("#menu").removeClass("green");
-	});
-	
 }); 
