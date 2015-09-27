16e21159aeaff5178af7a832dd01cdbb8fa3c2a4
Mustaghees Butt <renaldpirate@gmail.com>
2015-09-27 12:46:27 +0500

    Smooth scrolling
    Smooth scrolling to internal links was added with some other design
    implementations.

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,15 @@
 $(document).ready(function () {
 	
+	// Inertial scrolling
+	$('a[href^="#"]').on('click', function (e) {
+	    e.preventDefault();
+
+	    var target = this.hash;
+	    var $target = $(target);
+
+	    $('html, body').stop().animate({
+	        'scrollTop': $target.offset().top
+	    }, 900, 'swing');
+	});
+	
 }); 
