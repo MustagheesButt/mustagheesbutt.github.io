099019349c1be41e52eb76a5eb7a9b068453ffa1
Mustaghees Butt <renaldpirate@gmail.com>
2016-02-21 21:33:43 +0500

    Major SEO and design updates
    Used appropriate tags in home page

--- resources/scripts/graph.js
+++ resources/scripts/graph.js
@@ -1,116 +1,65 @@
-var divSkills, divKnows, divEnvs, divTools;
+google.charts.load('current', {'packages': ['corechart']});
+google.charts.setOnLoadCallback(drawChart);
 
-var dataSkills = [
-  { label: 'Web Designing', count: 86 }, 
-  { label: 'Web Development / Web Programming', count: 90 },
-  { label: 'Graphic Designing / Logo Designing', count: 65 },
-  { label: 'Programming', count: 73 },
-  { label: 'Networking', count: 63 },
-  { label: 'Content Writing', count: 56 }
-];
-var dataKnows = [
-  { label: 'PHP', count: 75 }, 
-  { label: 'SQL', count: 59 },
-  { label: 'HTML 5', count: 90 },
-  { label: 'CSS 3', count: 93 },
-  { label: 'JavaScript', count: 80 },
-  { label: 'C', count: 75 }
-];
-var dataEnvs = [
-  { label: 'Adobe Dreamweaver', count: 78 }, 
-  { label: 'SublimeText', count: 87 },
-  { label: 'Notepad++', count: 83 },
-  { label: 'Cloud9 (Online IDE)', count: 80 }
-];
-var dataTools = [
-  { label: 'Adobe Photoshop', count: 65 }, 
-  { label: 'Adobe Illustrator', count: 30 },
-  { label: 'Adobe Fireworks', count: 50 },
-  { label: 'AutoDesk Maya', count: 40 },
-  { label: 'Git / GitHub / GitLab (Control Version System)', count: 70 }
-];
-
-var dataset = [dataSkills, dataKnows, dataEnvs, dataTools];
-
-var width = 360;
-var height = 360;
-// Make height and width of graph responsive
-width = (window.innerWidth * 30) / 100; // makes width 30% of innerWidth
-height = width;
-var radius = Math.min(width, height) / 2;
-
-var color = d3.scale.category10();
-
-var legendRectSize = 18;
-var legendSpacing = 4;
-if (window.innerWidth < 480) {
-	legendRectSize = 9;
-	legendSpacing = 5;
-} else if (window.innerWidth < 670) {
-	legendRectSize = 12;
-	legendSpacing = 6;
-}
-
-$(document).ready(function () {
-	
-	divSkills = d3.select(".view-graph section:first-child .graph"),
-    divKnows = d3.select(".view-graph section:nth-child(2) .graph"),
-	divEnvs = d3.select(".view-graph section:nth-child(3) .graph"),
-	divTools = d3.select(".view-graph section:last-child .graph");
-	var divs = [divSkills, divKnows, divEnvs, divTools];
-	var legends = [".legend-1", ".legend-2", ".legend-3", ".legend-4"]
-	
-	var arc = d3.svg.arc()
-		  .outerRadius(radius);
-	var pie = d3.layout.pie()
-		  .value(function(d) { return d.count; })
-		  .sort(null);
-	
-	for (var i = 0; i < divs.length; i++) {
-		var svg = divs[i]
-			  .append('svg')
-			  .attr('width', width)
-			  .attr('height', height)
-			  .append('g')
-			  .attr('transform', 'translate(' + (width / 2) +  ',' + (height / 2) + ')');
-		var path = svg.selectAll('path')
-			  .data( pie(dataset[i]) )
-			  .enter()
-			  .append('path')
-			  .attr('d', arc)
-			  .attr('fill', function(d, i) { 
-				return color(d.data.label);
-		});
-		
-		// Domain starts to increase on each iteration, so instead of color.domain() we use pieLabels 
-		var pieLabels = dataset[i].map(function (d) {
-			  return d.label;
-		});
-		var legend = svg.selectAll('.legend')
-			  .data( pieLabels )
-			  .enter()
-			  .append('g')
-			  .attr('class', legends[i])
-			  .attr('transform', function(d, j) {
-				var height = legendRectSize + legendSpacing;
-				var offset =  height * pieLabels.length / 2;
-				var horz = width;
-				var vert = j * height - offset;
-				return 'translate(' + horz + ',' + vert + ')';
-			  });
-		legend.append('rect')
-			  .attr('width', legendRectSize)
-			  .attr('height', legendRectSize)
-			  .style('fill', color)
-			  .style('stroke', color);
-		legend.append('text')
-			  .attr('x', legendRectSize + legendSpacing)
-			  .attr('y', legendRectSize - legendSpacing/2)
-			  .attr('style', function () {
-				  var fs = legendRectSize;
-				  return "font-size: " + fs + "px";
-			  })
-			  .text(function(d) { return d; });
-	}
-
+$(window).on("resize", function (event) {
+    drawChart();
 });
+
+function drawChart() {
+	var dataSkills = google.visualization.arrayToDataTable([
+		['Skill', 'Index'],
+		['Web Designing', 86], 
+		['Web Development / Web Programming', 92] ,
+		['Graphic Designing / Logo Designing', 65],
+		['Writing', 90],
+		['Programming', 70],
+		['Networking', 63],
+	]);
+	var dataKnows = google.visualization.arrayToDataTable([
+		['Knows', 'Index'],
+		['PHP', 75], 
+		['SQL', 59],
+		['HTML 5', 90],
+		['CSS 3', 93],
+		['JavaScript', 80],
+		['C', 75]
+	]);
+	var dataEnvs = google.visualization.arrayToDataTable([
+		['Environment', 'Index'],
+		['Adobe Dreamweaver', 78], 
+		['SublimeText', 87],
+		['Notepad++', 83],
+		['Cloud9 (Online IDE)', 80]
+	]);
+	var dataTools = google.visualization.arrayToDataTable([
+		['Tool', 'Index'],
+		['Adobe Photoshop', 65], 
+		['Adobe Illustrator', 30],
+		['Adobe Fireworks', 50],
+		['AutoDesk Maya', 40],
+		['Git / GitHub / GitLab (Control Version System)', 70 ]
+	]);
+
+	var options = {
+	  width: "100%",
+	  height: "100%",
+	  chartArea: {
+            left: "3%",
+            top: "3%",
+            height: "94%",
+            width: "94%"
+        }
+	};
+	
+	var charts = document.getElementsByClassName("graph");
+	
+	var chart1 = new google.visualization.PieChart(charts[0]);
+	var chart2 = new google.visualization.PieChart(charts[1]);
+	var chart3 = new google.visualization.PieChart(charts[2]);
+	var chart4 = new google.visualization.PieChart(charts[3]);
+
+	chart1.draw(dataSkills, options);
+	chart2.draw(dataKnows, options);
+	chart3.draw(dataEnvs, options);
+	chart4.draw(dataTools, options);
+}
--- resources/scripts/script.js
+++ resources/scripts/script.js
@@ -9,162 +9,170 @@ var everythingLoaded = setInterval(function() {
 
 $(document).ready(function () {
 	
-	// Inertial scrolling
-	$('#contact h1, .scroll-down').on('click', function (e) {
-	    $('html, body').stop().animate({
-	        'scrollTop': e.target.offsetTop
-	    }, 900, 'swing');
-	});
-	
-	// insert &#215 in .block-false
-	$(".block-false").html("&#215;");
-	// insert &#10003 in .block-true
-	$(".block-true").html("&#10003;");
-	
-	(function($) {
-    
-     var allPanels = $('main > div > main').hide();
-     
-     $('main > div > h1').click(function() {
+	/**
+	 ** HOME PAGE
+	 **/
+	 if (window.location.toString().indexOf("/index.html")) {
+		 // Inertial scrolling
+		$('#contact h1, .scroll-down').on('click', function (e) {
+			$('html, body').stop().animate({
+				'scrollTop': e.target.offsetTop
+			}, 900, 'swing');
+		});
+		
+		// insert &#215 in .block-false
+		$(".block-false").html("&#215;");
+		// insert &#10003 in .block-true
+		$(".block-true").html("&#10003;");
+		
+		(function($) {
+		
+		 var allPanels = $('main > div > main').hide();
 		 
-		 if ( !$(this).parent().hasClass("active") ) {
-			$("main > div").removeClass("active");
-			
-			$(this).parent().addClass("active");
-			allPanels.slideUp(); 
-		 }
-		 
-		 $(this).parent().children("main").slideDown();
-		 return false;
-     });
+		 $('main > div > h1').click(function() {
+			 
+			 if ( !$(this).parent().hasClass("active") ) {
+				$("main > div").removeClass("active");
+				
+				$(this).parent().addClass("active");
+				allPanels.slideUp(); 
+			 }
+			 
+			 $(this).parent().children("main").slideDown();
+			 return false;
+		 });
 
-    }) (jQuery);
-	
-	/*  VIEW CONTROLLER - CONTROLES TOGGLING BETWEEN Table View & Graph View */
-	$(".view span:first-child").click(function () {
-		$(".view-table").css("display", "block");
-		$(".view-graph").css("display", "none");
+		}) (jQuery);
 		
-		$(this).addClass("active");
-		$(".view span:last-child").removeClass("active");
-	});
-	$(".view span:last-child").click(function () {
-		$(".view-graph").css("display", "block");
-		$(".view-table").css("display", "none");
-		
-		$(this).addClass("active");
-		$(".view span:first-child").removeClass("active");
-	});
-	
-	/* ADD PORTFOLIO ITEMS - VIA portfolio.json */
-	var dataArr = $.getJSON("portfolio.json")
-	.done(function (dataArr) {
-		console.log("Portfolio data loaded successfully.");
-		
-		var dev = $("#portfolio main .development");
-		var des = $("#portfolio main .design");
-		dataArr.forEach(function (element, index, array) {
-			if (element.category == "dev") {
-				dev.append("<div class='item'><div onclick='' class='item-thumb' style='background-image: url(" + element.thumbnail + 
-				"); '><div class='details'><div>SEE DETAILS</div></div></div><h3><span class='title'>" + element.title + 
-				"</span><a href=' " + element.link + " '><span class='fa fa-link'></span></a></h3></div>");
-			} else if (element.category == "des") {
-				des.append("<div class='item'><div onclick='' class='item-thumb' style='background-image: url(" + element.thumbnail + 
-				"); '><div class='details'><div>SEE DETAILS</div></div></div><h3><span class='title'>" + element.title + 
-				"</span><a href=' " + element.link + " '><span class='fa fa-link'></span></a></h3></div>");
-			}
+		/*  VIEW CONTROLLER - CONTROLES TOGGLING BETWEEN Table View & Graph View */
+		$(".view span:first-child").click(function () {
+			$(".view-table").css("display", "block");
+			$(".view-graph").css("display", "none");
+			
+			$(this).addClass("active");
+			$(".view span:last-child").removeClass("active");
+		});
+		$(".view span:last-child").click(function () {
+			$(".view-graph").css("display", "block");
+			$(".view-table").css("display", "none");
+			
+			$(this).addClass("active");
+			$(".view span:first-child").removeClass("active");
+			drawChart();
 		});
 		
-		/* PORTFOLIO DETAILS HANDLER */
-		$("div.details").on("click", function () {
-			// search for element in dataArr with same title
-			var currEle = $(this).parent().parent().children("h3").children(".title").html();
-			var ele;
-			for (var i = 0; i < dataArr.length; i++) {
-				if (dataArr[i].title == currEle) {
-					ele = dataArr[i];
-					break;
+		/* ADD PORTFOLIO ITEMS - VIA portfolio.json */
+		var dataArr = $.getJSON("portfolio.json")
+		.done(function (dataArr) {
+			console.log("Portfolio data loaded successfully.");
+			
+			var dev = $("#portfolio main .development");
+			var des = $("#portfolio main .design");
+			dataArr.forEach(function (element, index, array) {
+				if (element.category == "dev") {
+					dev.append("<div class='item'><div onclick='' class='item-thumb' style='background-image: url(" + element.thumbnail + 
+					"); '><div class='details'><div>SEE DETAILS</div></div></div><h3><span class='title'>" + element.title + 
+					"</span><a href=' " + element.link + " '><span class='fa fa-link'></span></a></h3></div>");
+				} else if (element.category == "des") {
+					des.append("<div class='item'><div onclick='' class='item-thumb' style='background-image: url(" + element.thumbnail + 
+					"); '><div class='details'><div>SEE DETAILS</div></div></div><h3><span class='title'>" + element.title + 
+					"</span><a href=' " + element.link + " '><span class='fa fa-link'></span></a></h3></div>");
 				}
-			}
-			
-			$(".details-box img").attr("src", ele.thumbnail);
-			$(".details-box .details-box-title span").html(ele.title);
-			$(".details-box .details-box-type span").html(ele.type);
-			$(".details-box .details-box-description").html(ele.description);
-			$(".details-box").addClass("active");
-		});
-		$(".details-box .close").on("click", function () {
-			$(".details-box").removeClass("active");
-		});
-	})
-	.fail(function () {
-		console.log("Portfolio data failed to load.");
-		$("#portfolio main .development, #portfolio main .design").append("<div>Something went wrong. Portfolio items could not be loaded!</div>");
-	});
-	
-	/* EMAIL HANDLER - CONTACT FORM */
-	var submitBtn = $("button[type='submit']");
-	$("#contact form").submit(function (e) {
-		e.preventDefault();
-		if (submitBtn.html() != "sent") {
-			submitBtn.html("Sending...").css({"background-color": "yellow", "color": "black"});
-			
-			$.ajax({
-				url: "//formspree.io/mustaghees99@live.com",
-				method: "POST",
-				data : {
-					Name: $("#contact form input[name='name']").val(),
-					Email: $("#contact form input[name='email']").val(),
-					Subject: $("#contact form input[name='_subject']").val(),
-					_subject: $("#contact form input[name='_subject']").val(),
-					Message: $("#contact form textarea").val()
-				},
-				dataType: "json"
-			})
-			.done(function ( response ) {
-				console.log(response.success);
-				submitBtn.html("sent").css({"background-color": "#1BB71B", "color": "#FFFFFF"});
 			});
-		} else {
-			console.log("already sent an email");
-		}
-	});
-	/* CONTACT FORM VALIDATOR */
-	$("form input").on("change keyup paste", function () {
-		if ( isValid( $(this).attr("type") ) == false ) {
-			$(this).addClass("wrong");
-		} else {
-			$(this).removeClass("wrong");
-		}
-	});
+			
+			/* PORTFOLIO DETAILS HANDLER */
+			$("div.details").on("click", function () {
+				// search for element in dataArr with same title
+				var currEle = $(this).parent().parent().children("h3").children(".title").html();
+				var ele;
+				for (var i = 0; i < dataArr.length; i++) {
+					if (dataArr[i].title == currEle) {
+						ele = dataArr[i];
+						break;
+					}
+				}
+				
+				$(".details-box img").attr("src", ele.thumbnail);
+				$(".details-box .details-box-title span").html(ele.title);
+				$(".details-box .details-box-type span").html(ele.type);
+				$(".details-box .details-box-description").html(ele.description);
+				$(".details-box").addClass("active");
+			});
+			$(".details-box .close").on("click", function () {
+				$(".details-box").removeClass("active");
+			});
+		})
+		.fail(function () {
+			console.log("Portfolio data failed to load.");
+			$("#portfolio main .development, #portfolio main .design").append("<div>Something went wrong. Portfolio items could not be loaded!</div>");
+		});
+		
+		/* EMAIL HANDLER - CONTACT FORM */
+		var submitBtn = $("button[type='submit']");
+		$("#contact form").submit(function (e) {
+			e.preventDefault();
+			if (submitBtn.html() != "sent") {
+				submitBtn.html("Sending...").css({"background-color": "yellow", "color": "black"});
+				
+				$.ajax({
+					url: "//formspree.io/mustaghees99@live.com",
+					method: "POST",
+					data : {
+						Name: $("#contact form input[name='name']").val(),
+						Email: $("#contact form input[name='email']").val(),
+						Subject: $("#contact form input[name='_subject']").val(),
+						_subject: $("#contact form input[name='_subject']").val(),
+						Message: $("#contact form textarea").val()
+					},
+					dataType: "json"
+				})
+				.done(function ( response ) {
+					console.log(response.success);
+					submitBtn.html("sent").css({"background-color": "#1BB71B", "color": "#FFFFFF"});
+				});
+			} else {
+				console.log("already sent an email");
+			}
+		});
+		/* CONTACT FORM VALIDATOR */
+		$("form input").on("change keyup paste", function () {
+			if ( isValid( $(this).attr("type") ) == false ) {
+				$(this).addClass("wrong");
+			} else {
+				$(this).removeClass("wrong");
+			}
+		});
+	 }
 	
 	/**
-	 ** PACKAGES - PAGE
+	 ** PRICING - PAGE
 	 **/
-
-	// Slider
-	var slideControls = $("section.marketing div.slide-controls span"),
-		   slides = $("section.marketing .slide"),
-		   scaleDown = 0.85,
-		   animSpeed = 0.75;
 	
-	TweenMax.to("section.marketing .slide:not(.active)", 0, {display: "none", left: 1500, scale: scaleDown});
+	if (window.location.toString().indexOf("pricing/pricing.html") != -1) {
+		// Slider
+		var slideControls = $("section.marketing div.slide-controls span"),
+			   slides = $("section.marketing .slide"),
+			   scaleDown = 0.85,
+			   animSpeed = 0.75;
+		
+		TweenMax.to("section.marketing .slide:not(.active)", 0, {display: "none", left: 1500, scale: scaleDown});
 
-	slideControls.click(function () {
-		slideControls.removeClass("active");
-		$(this).addClass("active");
-		
-		nthSlide = slideControls.index( $(this) );
-		// animate the current active slide to go out
-		TweenMax.to("section.marketing .slide.active", animSpeed, {scale: scaleDown});
-		TweenMax.to("section.marketing .slide.active", animSpeed, {display: "none", left: 1500, delay: animSpeed});
-		
-		slides.removeClass("active");
-		$(slides[nthSlide]).addClass("active");
-		// animate current slide to come in
-		TweenMax.to("section.marketing .slide.active", animSpeed, {display: "block", left: 0});
-		TweenMax.to("section.marketing .slide.active", animSpeed, {scale: 1, delay: animSpeed});
-	});
+		slideControls.click(function () {
+			slideControls.removeClass("active");
+			$(this).addClass("active");
+			
+			nthSlide = slideControls.index( $(this) );
+			// animate the current active slide to go out
+			TweenMax.to("section.marketing .slide.active", animSpeed, {scale: scaleDown});
+			TweenMax.to("section.marketing .slide.active", animSpeed, {display: "none", left: 1500, delay: animSpeed});
+			
+			slides.removeClass("active");
+			$(slides[nthSlide]).addClass("active");
+			// animate current slide to come in
+			TweenMax.to("section.marketing .slide.active", animSpeed, {display: "block", left: 0});
+			TweenMax.to("section.marketing .slide.active", animSpeed, {scale: 1, delay: animSpeed});
+		});
+	}
 	
 });
 
