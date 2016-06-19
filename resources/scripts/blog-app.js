9e85dd256a9ce21d66b30211d8aa0a2fcfd3f9cf
Mustaghees Butt <renaldpirate@gmail.com>
2016-06-19 08:10:41 +0500

    Disqus comment system
    Disqus comment system was added. CV/Resume was updated to include
    Spanish/espanol/castellano
    
    Some irrelevant code was removed from script.js

--- resources/scripts/blog-app.js
+++ resources/scripts/blog-app.js
@@ -50,6 +50,7 @@ blog.controller("BlogController", function ($scope, articlesService) {
 });
 blog.controller("PostController", function ($scope, $route, $routeParams) {
 	$("section > div").addClass("post full");
+	$("section > div").append("<div id=\"disqus_thread\"></div> <script> /** * RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS. * LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables */  var disqus_config = function () { this.page.url = window.location.href; this.page.identifier = window.location.hash; }; (function() { var d = document, s = d.createElement('script'); s.src = '//mustagheesblog.disqus.com/embed.js'; s.setAttribute('data-timestamp', +new Date()); (d.head || d.body).appendChild(s); })(); </script> <noscript>Please enable JavaScript to view the <a href=\"https://disqus.com/?ref_noscript\" rel=\"nofollow\">comments powered by Disqus.</a></noscript>");
 });
 blog.controller("ArchivesController", function ($scope, $route, $routeParams) {
 	$scope.order = "title";
